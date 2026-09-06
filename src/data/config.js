f93934e55889cf63611c1b637214efbc96aa0e8f
idlechara <3944601+idlechara@users.noreply.github.com>
2026-09-06 13:38:07 -0300

    add site config with runtime defaults

---
+++ src/data/config.js
@@ -0,0 +1,13 @@
+export const SITE_CONFIG = {
+  showTweaksButton: false,
+
+  defaults: {
+    starCount: 220,
+    starSpeed: 3,
+    starSize: 48,
+    palette: 'win95-cool',
+    showSparkles: true,
+    showMarquee: true,
+    contentWidth: 2000,
+  },
+};
